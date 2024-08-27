e8567056e7f4f25c2e7cb2005f0b022934133b66
Sarthak Srivastava <2016ceb1017@iitrpr.ac.in>
2024-08-28 01:04:08 +0530

    Grouping APIs for middleware to operate

--- routes/routes.go
+++ routes/routes.go
@@ -8,9 +8,12 @@ import (
 func RegisterRoutes(server *gin.Engine) {
 	server.GET("/events", getEvents)
 	server.GET("/events/:id", getEventByID)
-	server.POST("/events", middlewares.Authenticate, createEvent)
-	server.PUT("/events/:id", updateEvent)
-	server.DELETE("events/:id", deleteEvent)
+
+	authenticatedAPIs := server.Group("/")
+	authenticatedAPIs.Use(middlewares.Authenticate)
+	authenticatedAPIs.POST("/events", createEvent)
+	authenticatedAPIs.PUT("/events/:id", updateEvent)
+	authenticatedAPIs.DELETE("events/:id", deleteEvent)
 
 	server.POST("/signup", signup)
 	server.POST("/login", login)
